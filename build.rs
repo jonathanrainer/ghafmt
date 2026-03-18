6f67360d1b1c0e26c6139e5231fb3f96858b127c
Jonathan Rainer <jonathan@jonathan-rainer.com>
2026-03-18 06:25:01 +0000

    Run various formatters

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
     let version = std::env::var("CARGO_PKG_VERSION").unwrap_or_default();
-    let display = version.split_once('-').map_or(version.as_str(), |(_, pre)| pre);
+    let display = version
+        .split_once('-')
+        .map_or(version.as_str(), |(_, pre)| pre);
     println!("cargo:rustc-env=GHAFMT_VERSION_STRING={display}");
 }
